cd48329aae557cdb9fd3a088bf6a6f8c6458f558
Herbert V <herbert.videla@tektonlabs.com>
2016-12-10 17:16:22 -0500

    conflict fix

--- app/src/main/java/com/example/milagros/improof/MainActivity.java
+++ app/src/main/java/com/example/milagros/improof/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.milagros.improof;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageButton;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +17,22 @@ public class MainActivity extends AppCompatActivity {
         setTitle("Improof");
         ImageView avatar= (ImageView) findViewById(R.id.avatar);
         avatar.setImageResource(R.drawable.mendicant);
+        Button habitsSection = (Button) findViewById(R.id.habits);
 
+        habitsSection.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+                showHabitsSection(view);
+            }
+        });
     }
+
+    public void showHabitsSection(View view) {
+        Intent intent = new Intent(this, HabitsActivity.class);
+        startActivity(intent);
+    }
+
+
+
 }
