29987b3114ada1faa640312e37bc351d7c7fb6cc
Mily2709 <milyruihuis@gmail.com>
2016-12-11 02:03:34 -0500

    project activity completa
    el consumo completo

--- app/src/main/java/com/example/milagros/improof/Adapters/proyectosAdapter.java
+++ app/src/main/java/com/example/milagros/improof/Adapters/proyectosAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.example.milagros.improof.Model.Proyecto;
 import com.example.milagros.improof.R;
@@ -18,8 +19,11 @@ import java.util.ArrayList;
 
 public class proyectosAdapter extends ArrayAdapter<Proyecto> {
 
+    public static ArrayList<Proyecto> pros= new ArrayList<>();
+
     public proyectosAdapter(Context  context, ArrayList<Proyecto> proyectos) {
         super(context, R.layout.list_proyectview, proyectos);
+        pros = proyectos;
     }
 
     @Override
@@ -29,7 +33,17 @@ public class proyectosAdapter extends ArrayAdapter<Proyecto> {
         LayoutInflater inflater = (LayoutInflater) parent.getContext()
                 .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         itemView = inflater.inflate(R.layout.list_proyectview, null);
+        Proyecto currentPro =pros.get(position);
 
+        TextView nombre = (TextView) itemView.findViewById(R.id.namepro);
+        nombre.setText(currentPro.getName());
+        TextView hora = (TextView) itemView.findViewById(R.id.hour);
+        TextView min = (TextView) itemView.findViewById(R.id.min);
+        double time= currentPro.getTime();
+        int h= (int)(time/3600);
+        int m= (int)(time-h*3600);
+        hora.setText(h+"h");
+        min.setText(m+"m");
         return itemView;
     }
 
--- app/src/main/java/com/example/milagros/improof/MainActivity.java
+++ app/src/main/java/com/example/milagros/improof/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.milagros.improof;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.example.milagros.improof.Model.Proyecto;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +22,14 @@ public class MainActivity extends AppCompatActivity {
         setTitle("Improof");
         ImageView avatar= (ImageView) findViewById(R.id.avatar);
         avatar.setImageResource(R.drawable.mendicant);
+        Button proyecto=(Button) findViewById(R.id.proyects);
+        proyecto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent proyect_Activity = new Intent(getApplicationContext(), ProjectsActivity.class);
+                startActivity(proyect_Activity);
+            }
+        });
 
 
     }
